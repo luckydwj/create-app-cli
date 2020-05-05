9f3572573c4ab5e1cdc3ea14799f6ded8e52cc76
dwj <dwi@163.com>
2020-05-05 21:48:52 +0800

    配置

--- configs/config.dev.js
+++ configs/config.dev.js
@@ -1,6 +1,10 @@
 /**
  *  Created by daiwenjuan on 2020/5/5 下午6:22.
  */
+const path=require('path')
 module.exports = {
-  devtool: "inline-source-map"
+  devtool: "inline-source-map",
+  devServer:{
+    contentBase:path.resolve(__dirname, "../test/dist")
+  }
 };
