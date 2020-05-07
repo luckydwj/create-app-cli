aca78494cb80ed45e049bd74b69b1894c9478497
daiwenjuan <daiwenjuan668@163.com>
2020-05-07 13:54:10 +0800

    fix

--- test/src/index.js
+++ test/src/index.js
@@ -2,19 +2,14 @@
  *  Created by daiwenjuan on 2020/5/2 下午1:28.
  */
 import _ from "lodash";
-import "./style.css";
-import Icon from "./image/test.jpg";
-import printMe from "./print";
+import Print from "./print";
+
 function component() {
   var element = document.createElement("div");
-  var btn = document.createElement("button");
-
+  console.log("=====sdfsd======");
+  // lodash 是由当前 script 脚本 import 进来的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.onclick = Print.bind(null, "Hello webpack!");
 
   return element;
 }
